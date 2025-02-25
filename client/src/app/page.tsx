880d372b8cecd108fcbe42f1ece134e964554320
JerryLei <jerrylei.ui@gmail.com>
2025-02-25 03:32:17 -0800

    Jerry | adjust layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center -mt-16">
+    <div className="h-[calc(100vh-4rem)] w-full flex items-center justify-center">
       <button
         className="px-16 py-8 bg-[#25262b] rounded-xl text-white text-4xl font-bold
         hover:bg-[#2c2d31] hover:scale-105 hover:shadow-2xl
