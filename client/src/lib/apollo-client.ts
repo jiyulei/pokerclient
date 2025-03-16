ae4d886d020a999fd90fa0f96e6603c2f970272d
JerryLei <jerrylei.ui@gmail.com>
2025-03-16 03:58:35 -0700

    Jerry | Connect apollo client with production server

--- client/src/lib/apollo-client.ts
+++ client/src/lib/apollo-client.ts
@@ -3,17 +3,27 @@ import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-// GraphQL HTTP API 端点 (Fly.io 部署)
+// // GraphQL HTTP API 端点 (Fly.io 部署)
+// const httpLink = new HttpLink({
+//   uri:
+//     process.env.NEXT_PUBLIC_GRAPHQL_HTTP_URL || "http://localhost:4000/graphql",
+// });
+
+// // WebSocket API 端点 (Fly.io 部署)
+// const wsLink = new GraphQLWsLink(
+//   createClient({
+//     url:
+//       process.env.NEXT_PUBLIC_GRAPHQL_WS_URL || "ws://localhost:4000/graphql",
+//   })
+// );
+
 const httpLink = new HttpLink({
-  uri:
-    process.env.NEXT_PUBLIC_GRAPHQL_HTTP_URL || "http://localhost:4000/graphql",
+  uri: "https://pokerclient-production.up.railway.app/graphql",
 });
 
-// WebSocket API 端点 (Fly.io 部署)
 const wsLink = new GraphQLWsLink(
   createClient({
-    url:
-      process.env.NEXT_PUBLIC_GRAPHQL_WS_URL || "ws://localhost:4000/graphql",
+    url: "wss://pokerclient-production.up.railway.app/graphql",
   })
 );
 
